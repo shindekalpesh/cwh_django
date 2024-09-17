516bf34d284582c801e79610ede9993c63f05c63
Kalpesh <kalpesh@connou.app>
2024-09-17 19:13:57 +0530

    customized admin panel with changes in the text

--- hello/home/urls.py
+++ hello/home/urls.py
@@ -2,6 +2,11 @@ from django.contrib import admin
 from django.urls import path, include
 from home import views
 
+# To change admin portal display text
+admin.site.site_header = "CWH Icecream Admin"
+admin.site.site_title = "CWH Icecream Admin Portal"
+admin.site.index_title = "Welcome to CWH Icecream"
+
 urlpatterns = [
     path('', views.index, name='home'),
     path('about', views.about, name='about'),
