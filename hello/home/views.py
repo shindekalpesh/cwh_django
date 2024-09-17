075f6d72139e434732a6c30665bcb67ae6f188f1
Kalpesh <kalpesh@connou.app>
2024-09-17 19:33:44 +0530

    extended base.html to about, services, and contact

--- hello/home/views.py
+++ hello/home/views.py
@@ -10,10 +10,10 @@ def index(request):
     # return HttpResponse("This is home page!")
 
 def about(request):
-    return HttpResponse("This is about page!")
+    return render(request, 'about.html')
 
 def services(request):
-    return HttpResponse("This is services page!")
+    return render(request, 'services.html')
 
 def contact(request):
-    return HttpResponse("This is contact page!")
+    return render(request, 'contact.html')
